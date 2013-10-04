f38a4bca34ad85dfb62081815bf4cd7e404694c3
zhaozhiming <kingzzm1982@sina.com>
2013-10-04 20:12:02 +0800

    add list url function

--- src/main/java/com/github/spa/TaskController.java
+++ src/main/java/com/github/spa/TaskController.java
@@ -4,12 +4,18 @@ import com.github.spa.dto.Task;
 import com.github.spa.repo.TaskRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class TaskController {
@@ -27,9 +33,27 @@ public class TaskController {
     @RequestMapping(value = "/task/add", method = RequestMethod.POST)
     public String addTask(@RequestParam String taskName, String taskOwner, String createdDate) throws Exception {
         Task task = new Task(taskName, taskOwner, createdDate);
-        log.debug("task is null?" + (task == null));
         taskRepository.save(task);
         return "redirect:/pages/index.html";
     }
+
+    @RequestMapping(value = "/task/all", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String list() throws JSONException {
+        List<Task> tasks = taskRepository.findAll();
+
+        JSONArray tasksJson = new JSONArray();
+        for (Task task : tasks) {
+            JSONObject taskJson = new JSONObject();
+            taskJson.put("id", task.getId());
+            taskJson.put("name", task.getName());
+            taskJson.put("owner", task.getOwner());
+            taskJson.put("createdDate", task.getCreatedDate());
+            tasksJson.put(taskJson);
+        }
+        return tasksJson.toString();
+    }
+
 
 }
