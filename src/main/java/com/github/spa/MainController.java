c172eb57eea224999b20e4eedb5a0fd71da869ee
zhaozhiming <kingzzm1982@sina.com>
2014-02-18 22:31:08 +0800

    add upload file page

--- src/main/java/com/github/spa/MainController.java
+++ src/main/java/com/github/spa/MainController.java
@@ -5,15 +5,39 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 @Controller
 public class MainController {
     private static final Log log = LogFactory.getLog(MainController.class);
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() throws Exception {
-        log.debug("hello start");
-        return "hello";
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String handleFileUpload(@RequestParam("files[]") MultipartFile file) {
+        log.debug("upload start");
+        if (!file.isEmpty()) {
+            String name = file.getOriginalFilename();
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                log.debug("upload finish");
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload, because the file was empty.";
+        }
     }
 
 }
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,11 +1,24 @@
-var spa = angular.module('spa', ['ngRoute']);
-
-spa.config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/uploadFile', {templateUrl: 'pages/upload_file.html'})
-            .otherwise({redirectTo: '/home'});
-    }]);
-
 $(document).ready(function () {
     $('#sidebar').sidebar('toggle');
 });
+
+var spa = angular.module('spa', ['ngRoute', 'blueimp.fileupload']);
+
+spa.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/uploadFile', {templateUrl: 'pages/upload_file.html'})
+        .otherwise({redirectTo: '/home'});
+}]);
+
+spa.controller('FileUploadController', ['$scope', function($scope){
+    // Options you want to pass to jQuery File Upload e.g.:
+    console.log("test");
+    $scope.options = {
+        maxFileSize: 5000000,
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+    };
+
+    $scope.submit = function() {
+
+    }
+}]);
